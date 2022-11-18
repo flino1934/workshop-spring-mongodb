f344f080e1eea474f7ef629b44d513b7846d3344
flino1934 <f.lino1934@hotmail.com>
2022-11-18 14:33:27 -0300

    Estrutura inicial do projeto criada
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

---
+++ src/test/java/com/lino/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lino.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
