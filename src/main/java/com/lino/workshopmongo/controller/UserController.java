fb4ee0b37fae98f8debf2e7d5c6c42403c552343
flino1934 <f.lino1934@hotmail.com>
2022-11-19 13:11:00 -0300

    Criando o metodo insert do usuario
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/workshopmongo/controller/UserController.java
+++ src/main/java/com/lino/workshopmongo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.lino.workshopmongo.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,36 +8,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lino.workshopmongo.dto.UserDTO;
 import com.lino.workshopmongo.entity.User;
 import com.lino.workshopmongo.service.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll(){
-		
+	public ResponseEntity<List<UserDTO>> findAll() {
+
 		List<User> list = userService.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
-		
+
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
-		
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+
 		User user = userService.findById(id);
-		
+
 		return ResponseEntity.ok().body(new UserDTO(user));
-		
+
+	}
+
+	@PostMapping
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+
+		User user = userService.fromDTO(userDTO);
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
 	}
 
 }
--- src/main/java/com/lino/workshopmongo/service/UserService.java
+++ src/main/java/com/lino/workshopmongo/service/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lino.workshopmongo.dto.UserDTO;
 import com.lino.workshopmongo.entity.User;
 import com.lino.workshopmongo.repository.UserRepository;
 import com.lino.workshopmongo.service.exceptions.ObjectNotFoundException;
@@ -26,6 +27,18 @@ public class UserService {
 
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+	public User insert(User user) {
+
+		return userRepository.insert(user);
+
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 
 	}
 
