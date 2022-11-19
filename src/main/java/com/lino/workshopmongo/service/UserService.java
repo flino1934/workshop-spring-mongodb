28facd639f9cfdb054ae6e17d4d49123078978d8
flino1934 <f.lino1934@hotmail.com>
2022-11-19 11:53:50 -0300

    Implementando o metodo findById e o tratamento de exceção
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/workshopmongo/service/UserService.java
+++ src/main/java/com/lino/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.lino.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lino.workshopmongo.entity.User;
 import com.lino.workshopmongo.repository.UserRepository;
+import com.lino.workshopmongo.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,13 @@ public class UserService {
 	public List<User> findAll() {
 
 		return userRepository.findAll();
+
+	}
+
+	public User findById(String id) {
+
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
 	}
 
