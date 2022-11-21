d50c0271dac7af276adc49a412ef4e4973825168
flino1934 <f.lino1934@hotmail.com>
2022-11-21 12:34:26 -0300

    Adicionando comentarios ao posts
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

--- src/main/java/com/lino/workshopmongo/entity/Post.java
+++ src/main/java/com/lino/workshopmongo/entity/Post.java
@@ -1,13 +1,16 @@
 package com.lino.workshopmongo.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lino.workshopmongo.dto.AuthorDTO;
+import com.lino.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,7 +22,9 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
+
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 		// TODO Auto-generated constructor stub
@@ -71,6 +76,10 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
