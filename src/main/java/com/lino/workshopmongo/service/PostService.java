5cad57971dc83eecd000d2aa9827c0335c4a04e9
flino1934 <f.lino1934@hotmail.com>
2022-11-21 12:15:33 -0300

    Endpoint para carregar os posts de um usuario
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

---
+++ src/main/java/com/lino/workshopmongo/service/PostService.java
@@ -0,0 +1,27 @@
+package com.lino.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lino.workshopmongo.entity.Post;
+import com.lino.workshopmongo.repository.PostRepository;
+import com.lino.workshopmongo.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+
+
+}
